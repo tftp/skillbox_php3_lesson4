5cbd2848b0c3d3549a46f2e379755e7d9a64881a
OS <goloborodko@inbox.ru>
2021-04-16 21:39:57 +0300

    correct features in dev

--- app.php
+++ app.php
@@ -8,10 +8,7 @@ use Symfony\Component\Console\Application;
 $app = new Application();
 
 // ... register commands
-<<<<<<< HEAD
 $app->add(new App\SayHello());
-
-=======
 $app->add(new App\RepeatString());
->>>>>>> feature02
+
 $app->run();
